9945f1a8788e562d53bd2bf75337242bec8f0724
Ngurahbayu777 <163176869+Ngurahbayu777@users.noreply.github.com>
2024-03-13 02:39:29 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-onload = () =>{
-        document.body.classList.remove("container");
+window.onload = () => {
+    document.body.classList.remove("container"); // Menghapus kelas "container" dari elemen body
+    playMusic(); // Memanggil fungsi untuk memainkan musik
 };
+
+function playMusic() {
+    var audio = document.getElementById("backgroundMusic"); // Mengambil elemen audio berdasarkan ID
+    audio.play(); // Memainkan musik
+}
