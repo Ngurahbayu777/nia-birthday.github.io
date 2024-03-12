4aef6614c77fff321666819a54bb6b7116744593
Ngurahbayu777 <163176869+Ngurahbayu777@users.noreply.github.com>
2024-03-13 05:52:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-function playMusic() {
-    var audio = new Audio("https://j.top4top.io/m_299357awq1.mp3");
-    audio.play();
-
-    // Menampilkan popup
-    var popup = document.getElementById("popup");
-    popup.style.display = "block";
-
-    // Menyembunyikan popup setelah beberapa detik
-    setTimeout(function() {
-        popup.style.display = "none";
-    }, 3000); // Misalnya, dalam contoh ini, popup akan disembunyikan setelah 3 detik
-}
+onload = () =>{
+        document.body.classList.remove("container");
+};
