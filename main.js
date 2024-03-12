b66b3427185ea37ea36d87f50b8113facccc3144
Ngurahbayu777 <163176869+Ngurahbayu777@users.noreply.github.com>
2024-03-13 04:58:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 window.onload = () => {
     document.body.classList.remove("container"); // Menghapus kelas "container" dari elemen body
-    playMusic(); // Memanggil fungsi untuk memainkan musik
+    var playButton = document.getElementById("playButton"); // Mengambil elemen tombol play
+    playButton.addEventListener("click", playMusic); // Menambahkan event listener untuk memanggil fungsi playMusic saat tombol play diklik
 };
 
 function playMusic() {
     var audio = new Audio("https://j.top4top.io/m_299357awq1.mp3"); // Mendefinisikan objek audio dengan URL musik
     audio.play(); // Memainkan musik
 }
+
